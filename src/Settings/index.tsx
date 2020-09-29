4897d16de1ca83c89ae666d625c97b94d579f800
devinjameson <39350030+devinjameson@users.noreply.github.com>
2020-09-29 16:26:40 -0400

    Standardize naming of Screen types
    Why:
    Prior to this commit, we had some screen types using the pattern
    "XScreen" (e.g. "HomeScreen") and others using the pattern
    "XStackScreen" (e.g. "HomeStackScreen"). We would like the pattern we
    use to be consistent.
    
    This commit:
    - Update all Screen types to use the pattern "XStackScreen" as in
    "HomeStackScreen".

--- src/Settings/index.tsx
+++ src/Settings/index.tsx
@@ -8,8 +8,8 @@ import { getLocalNames } from "../locales/languages"
 import {
   useStatusBarEffect,
   Stacks,
-  ModalScreens,
-  SettingsScreens,
+  ModalStackScreens,
+  SettingsStackScreens,
 } from "../navigation"
 import { useConfigurationContext } from "../ConfigurationContext"
 import { ListItem, ListItemSeparator } from "../components"
@@ -42,13 +42,13 @@ const Settings: FunctionComponent = () => {
 
   const handleOnPressSelectLanguage = () => {
     navigation.navigate(Stacks.Modal, {
-      screen: ModalScreens.LanguageSelection,
+      screen: ModalStackScreens.LanguageSelection,
     })
   }
 
   const handleOnPressHowTheAppWorks = () => {
     navigation.navigate(Stacks.Modal, {
-      screen: ModalScreens.HowItWorksReviewFromSettings,
+      screen: ModalStackScreens.HowItWorksReviewFromSettings,
     })
   }
 
@@ -59,7 +59,7 @@ const Settings: FunctionComponent = () => {
   }
   const legal: SettingsListItem = {
     label: t("screen_titles.legal"),
-    onPress: () => navigation.navigate(SettingsScreens.Legal),
+    onPress: () => navigation.navigate(SettingsStackScreens.Legal),
     icon: Icons.Document,
   }
   const howTheAppWorks: SettingsListItem = {
@@ -69,7 +69,7 @@ const Settings: FunctionComponent = () => {
   }
   const debugMenu: SettingsListItem = {
     label: "EN Debug Menu",
-    onPress: () => navigation.navigate(SettingsScreens.ENDebugMenu),
+    onPress: () => navigation.navigate(SettingsStackScreens.ENDebugMenu),
     icon: Icons.Document,
   }
 
