9c793994d7b37de62a628066d4c92a206f78e0e5
Bryce <bsaito88@gmail.com>
2020-10-20 15:11:56 -1000

    Changed colors for onboarding.

--- src/configuration/brandColors.ts
+++ src/configuration/brandColors.ts
@@ -2,10 +2,10 @@
 export const primary100 = "#36628c" // mod asBlue
 export const primary110 = "#4754c5"
 export const primary125 = "#1a4267" // asBlue
-export const primary150 = "#192591"
+export const primary150 = "#123657"
 
 // Secondary
 export const secondary10 = "#f8f8ff"
-export const secondary50 = "#e5e7fa"
-export const secondary75 = "#d3d7f8"
+export const secondary50 = "#e6f0fa"
+export const secondary75 = "#d2e6f7"
 export const secondary100 = "#6ea0cc" // light asBlue
