d4e7c34472ff0a22253ef74cd4bdb28124deb2ba
Bryce <bsaito88@gmail.com>
2020-10-21 19:26:48 -1000

    Removed purples.

--- src/configuration/brandColors.ts
+++ src/configuration/brandColors.ts
@@ -1,11 +1,11 @@
 // Primary
-export const primary100 = "#36628c" // mod asBlue
-export const primary110 = "#4754c5"
+export const primary100 = "#3572ab" // mod asBlue
+export const primary110 = "#386894"
 export const primary125 = "#1a4267" // asBlue
 export const primary150 = "#123657"
 
 // Secondary
-export const secondary10 = "#f8f8ff"
+export const secondary10 = "#f5faff"
 export const secondary50 = "#e6f0fa"
 export const secondary75 = "#d2e6f7"
 export const secondary100 = "#6ea0cc" // light asBlue
