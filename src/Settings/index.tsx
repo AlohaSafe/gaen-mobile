7a60391ee87321adb6c4f6d1f7001fd60b796064
johnschoeman <johnschoeman1617@gmail.com>
2020-11-30 11:40:00 -0800

    Remove staging env var (#719)
    Why:
    Currently we have a staging environment variable in .env.bt which is
    used to determine if error reporting and the debug menu should be shown.
    We would like to explicitly enable error reporting and the debug menu
    with descriptive configuration variable names, rather than inferring the
    behavior based off a notion of 'staging' as we do not have single
    staging environment for this project.
    
    This commit:
    Removes env.STAGING and adds env.ENABLE_ERROR_REPORTING and
    env.DISPLAY_DEBUG_MENU

--- src/Settings/index.tsx
+++ src/Settings/index.tsx
@@ -52,6 +52,7 @@ const Settings: FunctionComponent = () => {
   const { healthAuthorityName } = useCustomCopy()
 
   const { languageName } = useLocaleInfo()
+<<<<<<< HEAD
 
   const CLICKS_TO_ENABLE_EN_DEBUG_MENU = 10
 
@@ -64,6 +65,9 @@ const Settings: FunctionComponent = () => {
   }, [clickCount, showDebugMenu])
 
   const incrementClickCount = () => setClickCount(clickCount + 1)
+=======
+  const showDebugMenu = env.DISPLAY_DEBUG_MENU === "true" || __DEV__
+>>>>>>> Remove staging env var (#719)
 
   const handleOnPressSelectLanguage = () => {
     navigation.navigate(ModalStackScreens.LanguageSelection)
